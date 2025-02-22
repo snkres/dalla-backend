61dae3c43511b0c76b7c41a26ccda55f65db4248
Mahmoud Galal <ssss.aaa2002@gmail.com>
2025-02-22 04:47:12 +0200

    lint issues

--- src/professionals/professional.controller.ts
+++ src/professionals/professional.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, HttpStatus, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  HttpStatus,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ProfessionalsService } from './professional.service';
 import { CustomHttpException } from '@/shared/exceptions/custom-http-exception';
 import { ResponseUtil } from '@/shared/utils/response.util';
@@ -9,24 +15,26 @@ export class ProfessionalsController {
   constructor(private readonly professionalsService: ProfessionalsService) {}
 
   @Post('parse-resume')
-    @UseInterceptors(
-      FileInterceptor('file', {
-        limits: {
-          fileSize: 1024 * 1024 * 10, // 10MB
-          files: 1,
-        },
-      }),
-    )
-    async parseResume(@UploadedFile() file: Express.Multer.File) {
-      try {
-        const parsedResume = await this.professionalsService.parseResume(file.buffer);
-        return ResponseUtil.success(parsedResume, 'Resume parsed successfully');
-      } catch (err) {
-        return new CustomHttpException(
-          err.message,
-          err.errors,
-          err.status ||  HttpStatus.BAD_REQUEST,
-        );
-      }
+  @UseInterceptors(
+    FileInterceptor('file', {
+      limits: {
+        fileSize: 1024 * 1024 * 10, // 10MB
+        files: 1,
+      },
+    }),
+  )
+  async parseResume(@UploadedFile() file: Express.Multer.File) {
+    try {
+      const parsedResume = await this.professionalsService.parseResume(
+        file.buffer,
+      );
+      return ResponseUtil.success(parsedResume, 'Resume parsed successfully');
+    } catch (err) {
+      return new CustomHttpException(
+        err.message,
+        err.errors,
+        err.status || HttpStatus.BAD_REQUEST,
+      );
     }
+  }
 }
