e5d1b36b1714e623adcf3d182564cd3387d337e5
Mahmoud Galal <ssss.aaa2002@gmail.com>
2025-02-23 03:34:02 +0200

    making fields optional

--- src/companies/validation/onboarding.validation.ts
+++ src/companies/validation/onboarding.validation.ts
@@ -1,7 +1,6 @@
 import { CompanyProfileMeta } from '@/shared/types/company.types';
 import {
   IsArray,
-  IsNotEmpty,
   IsObject,
   IsOptional,
   IsString,
@@ -10,50 +9,41 @@ import {
 
 export class OnboardingValidation {
   @IsString()
-  @IsNotEmpty()
   @IsOptional()
   location: string;
 
   @IsArray()
   @IsString({ each: true })
-  @IsNotEmpty()
   @IsOptional()
   areas: string[];
 
   @IsArray()
   @IsString({ each: true })
-  @IsNotEmpty()
   @IsOptional()
   goals: string[];
 
   @IsArray()
   @IsString({ each: true })
-  @IsNotEmpty()
   @IsOptional()
   targetIndustries: string[];
 
   @IsUrl()
-  @IsNotEmpty()
   @IsOptional()
   website: string;
 
   @IsString()
-  @IsNotEmpty()
   @IsOptional()
   headline: string;
 
   @IsString()
-  @IsNotEmpty()
   @IsOptional()
   bio: string;
 
   @IsString()
-  @IsNotEmpty()
   @IsOptional()
   logo: string;
 
   @IsObject()
-  @IsNotEmpty()
   @IsOptional()
   meta: CompanyProfileMeta;
 }
