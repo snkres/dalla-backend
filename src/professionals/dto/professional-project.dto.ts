e7946dd70a662755c540f8cb0ad2591674b2f658
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2025-03-11 07:06:06 +0200

    fix: lint

--- src/professionals/dto/professional-project.dto.ts
+++ src/professionals/dto/professional-project.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsArray,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-  IsUrl,
-} from 'class-validator';
+import { IsArray, IsNotEmpty, IsString, IsUrl } from 'class-validator';
 
 export class ProfessionalProjectDto {
   @IsString()
