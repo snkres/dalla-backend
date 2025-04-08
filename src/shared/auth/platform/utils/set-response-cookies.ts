dbba5c06c8dadc435734ae2d58b3346d0c2e5397
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2025-04-08 14:48:42 +0200

    chore(auth): set cookies sameSite to "lax"

--- src/shared/auth/platform/utils/set-response-cookies.ts
+++ src/shared/auth/platform/utils/set-response-cookies.ts
@@ -10,6 +10,7 @@ export default function setResponseCookies(
   res.cookie('refresh_token', payload.refresh_token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'lax',
     domain:
       process.env.NODE_ENV !== 'development' ? process.env.domain : 'localhost',
   });
@@ -17,6 +18,7 @@ export default function setResponseCookies(
   res.cookie('access_token', payload.access_token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'lax',
     domain:
       process.env.NODE_ENV !== 'development' ? process.env.domain : 'localhost',
   });
