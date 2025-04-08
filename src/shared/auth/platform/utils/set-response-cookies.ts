4d69ad79f54ded5d38ff2d59f13a889c68a94645
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2025-04-08 19:45:58 +0200

    fix(auth): change sameSite cookie attribute to 'none' for cross-site compatibility

--- src/shared/auth/platform/utils/set-response-cookies.ts
+++ src/shared/auth/platform/utils/set-response-cookies.ts
@@ -10,7 +10,7 @@ export default function setResponseCookies(
   res.cookie('refresh_token', payload.refresh_token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',
-    sameSite: 'lax',
+    sameSite: 'none',
     domain:
       process.env.NODE_ENV !== 'development' ? process.env.domain : 'localhost',
   });
@@ -18,7 +18,7 @@ export default function setResponseCookies(
   res.cookie('access_token', payload.access_token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development',
-    sameSite: 'lax',
+    sameSite: 'none',
     domain:
       process.env.NODE_ENV !== 'development' ? process.env.domain : 'localhost',
   });
