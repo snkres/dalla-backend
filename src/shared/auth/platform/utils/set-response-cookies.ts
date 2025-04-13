9169b5660cd42edc6c8accff16fc8824cd3d9acd
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2025-04-14 01:15:32 +0200

    no cookies attributes

--- src/shared/auth/platform/utils/set-response-cookies.ts
+++ src/shared/auth/platform/utils/set-response-cookies.ts
@@ -9,8 +9,8 @@ export default function setResponseCookies(
   options?: CookieOptions,
 ): void {
   res.cookie('refresh_token', payload.refresh_token, {
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    // httpOnly: true,
+    // maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
     // secure: process.env.NODE_ENV !== 'development',
     // domain:
     // process.env.NODE_ENV !== 'development' ? process.env.domain : 'localhost',
@@ -18,8 +18,8 @@ export default function setResponseCookies(
   });
 
   res.cookie('access_token', payload.access_token, {
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    // httpOnly: true,
+    // maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
     // secure: process.env.NODE_ENV !== 'development',
     // domain:
     //   process.env.NODE_ENV !== 'development' ? process.env.domain : 'localhost',
