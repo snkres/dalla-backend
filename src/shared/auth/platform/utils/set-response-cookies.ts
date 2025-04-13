be53be119e032278cee53a9fae4c4808ef3baa0d
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2025-04-14 00:20:39 +0200

    commented cookies domain attribute

--- src/shared/auth/platform/utils/set-response-cookies.ts
+++ src/shared/auth/platform/utils/set-response-cookies.ts
@@ -11,16 +11,16 @@ export default function setResponseCookies(
   res.cookie('refresh_token', payload.refresh_token, {
     httpOnly: true,
     // secure: process.env.NODE_ENV !== 'development',
-    domain:
-      process.env.NODE_ENV !== 'development' ? process.env.domain : 'localhost',
+    // domain:
+    //   process.env.NODE_ENV !== 'development' ? process.env.domain : 'localhost',
     ...options,
   });
 
   res.cookie('access_token', payload.access_token, {
     httpOnly: true,
     // secure: process.env.NODE_ENV !== 'development',
-    domain:
-      process.env.NODE_ENV !== 'development' ? process.env.domain : 'localhost',
+    // domain:
+    //   process.env.NODE_ENV !== 'development' ? process.env.domain : 'localhost',
     ...options,
   });
 
